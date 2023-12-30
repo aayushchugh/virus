b3c2eb5a6a468e8ff6e4396f555d035977b694b9
Ayush Chugh <ayushchugh2006@gmail.com>
2023-12-30 11:25:51 +0530

    delete directory with contents

--- virus.py
+++ virus.py
@@ -5,6 +5,11 @@ path = os.getcwd()
 
 # Delete the directory
 def delete_dir(directory):
+  # Delete files in directory
+  files = os.listdir(directory)
+  for file in files:
+    os.remove(os.path.join(directory, file))
+  
   os.rmdir(directory)
 
-delete_dir(path + "/test")
+delete_dir(os.path.join(path, 'test'))
